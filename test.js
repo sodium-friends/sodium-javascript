95bcdb89eabf001c30dc2d314091c3983fcfb4e0
Nick Peihl <nickp@sanjuanco.com>
2017-11-07 09:45:56 -0800

    Add test for randombytes in web worker context

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
 require('sodium-test')(require('.'))
 
-if (typeof window !== 'undefined') window.close()
+if (typeof window !== 'undefined') {
+  var test = require('tape')
+  var arrConst = new Uint8Array(16)
+  test('randombytes works in web worker context', function (t) {
+    var work = require('webworkify')
+    var w = work(require('./test-worker.js'))
+    w.addEventListener('message', function (e) {
+      var arr = e.data[0]
+      t.notEqual(arrConst, arr, '')
+      t.end()
+      window.close()
+    })
+    var arr = new Uint8Array(16)
+    w.postMessage([arr])
+  })
+}
