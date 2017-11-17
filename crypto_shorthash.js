f7de366eeca06c4c0a9b48ab76b6c37aac9273fe
Mathias Buus <mathiasbuus@gmail.com>
2017-11-17 12:07:52 +0100

    fix shorthash_ready being deprecated

--- crypto_shorthash.js
+++ crypto_shorthash.js
@@ -4,7 +4,7 @@ exports.crypto_shorthash_PRIMITIVE = 'siphash24'
 exports.crypto_shorthash_BYTES = siphash.BYTES
 exports.crypto_shorthash_KEYBYTES = siphash.KEYBYTES
 exports.crypto_shorthash_WASM_SUPPORTED = siphash.WASM_SUPPORTED
-exports.crypto_shorthash_ready = siphash.ready
+exports.crypto_shorthash_WASM_LOADED = siphash.WASM_LOADED
 exports.crypto_shorthash = shorthash
 
 function shorthash (out, data, key, noAssert) {
