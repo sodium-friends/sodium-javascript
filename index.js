5dd22a09570d9091571284ca91896d7db4fed600
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-05-04 21:11:30 +0200

    module: crypto_onetimeauth

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ var sodium = module.exports
 var randombytes = require('./randombytes').randombytes
 
 
-function crypto_onetimeauth(out, outpos, m, mpos, n, k) {
-  var s = new poly1305(k);
-  s.update(m, mpos, n);
-  s.finish(out, outpos);
-  return 0;
-}
-
-function crypto_onetimeauth_verify(h, hpos, m, mpos, n, k) {
-  var x = new Uint8Array(16);
-  crypto_onetimeauth(x,0,m,mpos,n,k);
-  return crypto_verify_16(h,hpos,x,0);
-}
-
-
 
 
 function crypto_box_keypair(pk, sk) {
