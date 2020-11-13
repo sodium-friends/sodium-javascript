ca32540960ddbfd31ecd8314440529216cdaf862
Mathias Buus <mathiasbuus@gmail.com>
2020-11-13 16:46:06 +0100

    load the sink just in time

--- memory.js
+++ memory.js
@@ -1,19 +1,26 @@
 /* eslint-disable camelcase */
-var MessageChannel = global.MessageChannel
-if (MessageChannel == null) ({ MessageChannel } = require('worker' + '_threads'))
 
 function sodium_malloc (n) {
   return new Uint8Array(n)
 }
 
-const sink = new MessageChannel()
 function sodium_free (n) {
   sodium_memzero(n)
-  sink.port1.postMessage(n.buffer, [n.buffer])
+  loadSink().port1.postMessage(n.buffer, [n.buffer])
 }
 
 function sodium_memzero (arr) {
   arr.fill(0)
+}
+
+var sink
+
+function loadSink () {
+  if (sink) return sink
+  var MessageChannel = global.MessageChannel
+  if (MessageChannel == null) ({ MessageChannel } = require('worker' + '_threads'))
+  sink = new MessageChannel()
+  return sink
 }
 
 module.exports = {
